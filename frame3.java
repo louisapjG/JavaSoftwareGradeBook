12edcd5ea2147fa51b1cb93716d0a3b5377f06c5
Louis Gobin <louis.gobin08@gmail.com>
2016-10-03 11:04:31 -0400

    Inclusion of JtextFields
    Include Jtext Fields, problem formatting them.

--- frame3.java
+++ frame3.java
@@ -7,9 +7,11 @@ Organize the middle (table) panel in the frame
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class frame3 extends frame2
 {
+	int StudentNumber=25;
 	public static void main (String args[])
         {
                 frame thisclass = new frame3();
@@ -24,7 +26,7 @@ public class frame3 extends frame2
 	public void StrucAdd (JFrame f)
 	{
 		super.StrucAdd(f);
-
+		//Add The buttons at the top of middle panel
 		JButton[] headers;
 		String[] headersName={"Student Name","ID#","Quizz","HomeWork","Exams","Average","Comments","Bonus points"};
 		headers=new JButton[headersName.length];
@@ -34,14 +36,44 @@ public class frame3 extends frame2
 		for(int count=0;count<headersName.length;count++)
                 {
                         headers[count]=new JButton(headersName[count]);
-			c.fill= GridBagConstraints.VERTICAL;
+			c.fill= GridBagConstraints.HORIZONTAL;
 			//c.ipadx=0;
 			//c.ipady=0;
-			c.weightx=0.2;
+			if(count==7)
+			{
+				c.weightx=0.5;
+			}
+			else
+			{
+				c.weightx=0.2;
+			}
 			//c.anchor=GridBagConstraints.PAGE_START;
 			c.gridx=count;
 			c.gridy=0;
 			middlePanel.add(headers[count],c);
                 }
+
+		//Add the board itself
+		//List<JTextField> textF= new ArrayList<JTextField>();
+		//List<ArrayList<ArrayList>> ListTextF = new ArrayList<JTextField>();
+		for (int Row =0; Row<StudentNumber;Row++)
+		{
+			for (int Col =0; Col<9; Col++)
+			{
+				JTextField Temp = new JTextField(13);
+				c.fill=GridBagConstraints.VERTICAL;
+				c.gridx=Col;
+				c.gridy=Row;
+				c.ipadx=0;
+				c.ipady=0;
+				c.weightx=0.2;
+				if(Col==7)
+				{
+					c.weightx=0.5;
+				}
+				middlePanel.add(Temp,c);
+			}
+		}
+		
 	}
 }
